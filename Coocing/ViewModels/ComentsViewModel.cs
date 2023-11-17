37774bfe081357bf4a5ef9cc4596720a8f157021
Mykola Krasko <mikokrsk@gmail.com>
2023-11-17 16:33:44 +0200

    hot fix

---
+++ Coocing/ViewModels/ComentsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Coocing.ViewModels
+{
+    public class ComentsViewModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string AppUserId { get; set; }
+        public int RecipesId { get; set; }
+    }
+}
