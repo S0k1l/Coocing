f7662e9a01ee1d0d181e62eb76fcd079c34d8013
S0k1l <martynuky16@gmail.com>
2023-11-17 14:10:11 +0200

    Add project files.

---
+++ Coocing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coocing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
